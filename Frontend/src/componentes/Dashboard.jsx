1cddfc9370a45a79e78ab4bc1858df13c8c057fd
RodrigoQuiroz <101417762+RodrigoQuiroz@users.noreply.github.com>
2024-04-29 00:15:35 -0400

    soluciones merge

--- Frontend/src/componentes/Dashboard.jsx
+++ Frontend/src/componentes/Dashboard.jsx
@@ -108,7 +108,6 @@ const Dashboard = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div>
       <h1 className="h1 mb-6 mt-6 items-center text-white relative text-center z-10 text-5xl">
         {t("dashboard")}
@@ -138,50 +137,6 @@ const Dashboard = () => {
             Certificación 3
           </li>
         </ul>
-=======
-    <Section
-      className="pt-[12rem] -mt-[5.25] md:-mt-0 bg-slate-100"
-      crosses
-      crossesOffset="lg:translate-y-[5.25rem]"
-      customPaddings
-      id="dashboard"
-    >
-      <div className="container relative w-full">
-        <h1 className="h1 mb-6 mt-6 items-center text-white relative text-center z-10 text-5xl">
-          {t("dashboard")}
-        </h1>
-        <div>
-          <span className="text-xl font-bold">
-            {" "}
-            Sube una certificación de la lista:
-          </span>
-          <ul className="mt-6 space-y-4 border-2 p-2 text-center bg-slate-50 text-white w-64 rounded-lg">
-            <li
-              className="cursor-pointer border bg-slate-700 p-2 rounded-lg hover:opacity-80"
-              onClick={() => togglePopup("Certificación 1")}
-            >
-              Certificación 1
-            </li>
-            <li
-              className="cursor-pointer border bg-slate-700 p-2 rounded-lg hover:opacity-80"
-              onClick={() => togglePopup("Certificación 2")}
-            >
-              Certificación 2
-            </li>
-            <li
-              className="cursor-pointer border bg-slate-700 p-2 rounded-lg hover:opacity-80"
-              onClick={() => togglePopup("Certificación 3")}
-            >
-              Certificación 3
-            </li>
-          </ul>
-        </div>
-        <Popup
-          show={showPopup}
-          onClose={togglePopup}
-          certificateName={selectedCertificate}
-        />
->>>>>>> abf25f7274598e25e73249baf8c0ea373a456863
       </div>
       <Popup
         show={showPopup}
